e2b51c9d7be69198b7407bd54555d75d0cb168df
Milcah Centeno <milcahcenteno@Milcahs-MacBook-Air.local>
2018-06-22 16:54:18 -0700

    set up project structure

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 /*----- constants -----*/
+
 /*----- app's state (variables) -----*/
+var board, score;
+
 /*----- cached element references -----*/
+var scoreEl = document.querySelector('h1');
+
 /*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', handleIncScore);
+
 /*----- functions -----*/
+function initialize() {
+    board = [null, null, null];
+    score = 0;
+
+    render(); // the last thing it does
+}
+
+// responsible for transfering all state to the DOM
+function render() {
+    scoreEl.textContent = score;
+}
+
+function handleIncScore() {
+    score += 1;
+    
+    render();
+}
+
+initialize();
