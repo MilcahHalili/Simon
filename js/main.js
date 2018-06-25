e60e66ba8657ab00e1db10a2cbdd1dbe415193a7
Milcah Centeno <milcahcenteno@Milcahs-MacBook-Air.local>
2018-06-25 09:59:31 -0700

    created style

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
-var board, score;
+var board, score, tempColor,runMemory, tempo;
 
 /*----- cached element references -----*/
-var scoreEl = document.querySelector('h1');
+var countEl = document.querySelector('h1');
+var running = false;
+var memoryArray = [];
+var memoryArrayCounter = 0;
+var userArray = [];
+var userArrayCounter = 0;
+var levelCount = 1;
+var matchingArrays = true;
+
 
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', handleIncScore);
+document.getElementById('startBtn').addEventListener('click', function() { 
+    handleUpdateScore(1);
+});
 
 /*----- functions -----*/
 function initialize() {
-    board = [null, null, null];
-    score = 0;
+    board = [];
+    turn = 0;
 
     render(); // the last thing it does
 }
 
 // responsible for transfering all state to the DOM
 function render() {
-    scoreEl.textContent = score;
+    countEl.textContent = turn;
 }
 
-function handleIncScore() {
-    score += 1;
-    
+function handleUpdateScore(nextTurn) {
+    turn += nextTurn;
+
     render();
 }
 
