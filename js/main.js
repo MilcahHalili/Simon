d898b5bab091c365a8d8299dd1a7c9f93fb18458
Milcah Centeno <milcahcenteno@Milcahs-MacBook-Air.local>
2018-06-26 10:44:12 -0700

    create button functionality

--- js/main.js
+++ js/main.js
@@ -1,41 +1,89 @@
 /*----- constants -----*/
+var circles = [0, 1, 2, 3];
 
 /*----- app's state (variables) -----*/
-var board, score, tempColor,runMemory, tempo;
+var running = false;
+var circleSequence = [];
 
 /*----- cached element references -----*/
-var countEl = document.querySelector('h1');
-var running = false;
-var memoryArray = [];
-var memoryArrayCounter = 0;
-var userArray = [];
-var userArrayCounter = 0;
-var levelCount = 1;
-var matchingArrays = true;
-
+var turnEl = document.querySelector('h1');
+var circBtns = document.querySelectorAll('.circ');
+var startBtn = document.getElementById('startBtn');
+var onBtn = document.getElementById('onBtn');
+var offBtn = document.getElementById('offBtn');
+var rand = Math.floor(Math.random()*circles.length);
 
 /*----- event listeners -----*/
-document.getElementById('startBtn').addEventListener('click', function() { 
-    handleUpdateScore(1);
-});
+
+onBtn.addEventListener('click', gameOn);
+
+startBtn.addEventListener('click', startGame);
+
+offBtn.addEventListener('click', gameOff);
+
+// circBtns.addEventListener('click', )
 
 /*----- functions -----*/
 function initialize() {
     board = [];
     turn = 0;
-
+    startBtn.disabled = true;
+    randomColor();
     render(); // the last thing it does
 }
 
 // responsible for transfering all state to the DOM
 function render() {
-    countEl.textContent = turn;
+    turnEl.textContent = turn;
 }
 
-function handleUpdateScore(nextTurn) {
+function handleUpdateTurn(nextTurn) {
     turn += nextTurn;
-
     render();
 }
 
 initialize();
+
+function gameOn() {
+    startBtn.disabled = false;
+}
+
+function gameOff() {
+    startBtn.disabled = true;
+    onBtn.disabled = false;
+    initialize();
+}
+
+function startGame() {
+    startBtn.disabled = true;
+    onBtn.disabled = true;
+    running = true;
+    handleUpdateTurn(1);
+    gameSequence();
+}
+
+function randomColor() {
+    var rand = Math.floor(Math.random()*circles.length);
+    circleSequence.push(circBtns[rand]);
+    // render();
+}
+
+function gameSequence() {
+    circleSequence.forEach(function(elem, idx) {
+        setTimeout(function() {
+            var intervalId = setInterval(function() {
+            if (elem.className === 'circ') {
+                elem.className += ' activated';    
+            } else {
+                elem.className = 'circ';
+                clearInterval(intervalId);
+            }    
+            }, 1000);
+        }, 1000 + (idx * 1000));
+        })
+    render();
+}
+
+// function playerSequence() {
+    
+// }
