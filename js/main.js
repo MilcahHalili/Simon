cf1e58ce8be425cbbf52f60ad425c7afa4846d54
Milcah Centeno <milcahcenteno@Milcahs-MacBook-Air.local>
2018-06-28 13:28:02 -0700

    add sound to turn sequence and player clicks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /*----- constants -----*/
-// var sounds = [];
-// var player = new Audio();
+var sounds = ['file:///Users/milcahcenteno/code/WDI-DT-59/projects/Simon/sounds/simonSound1.mp3', 'file:///Users/milcahcenteno/code/WDI-DT-59/projects/Simon/sounds/simonSound2.mp3', 'file:///Users/milcahcenteno/code/WDI-DT-59/projects/Simon/sounds/simonSound3.mp3', 'file:///Users/milcahcenteno/code/WDI-DT-59/projects/Simon/sounds/simonSound4.mp3'];
+var player = new Audio();
+player.src = sounds;
 
 /*----- app's state (variables) -----*/
 var circleSequence = [];
@@ -10,15 +11,33 @@ var gameOver, ignoreClicks;
 /*----- cached element references -----*/
 var turnEl = document.querySelector('h1');
 var startBtn = document.getElementById('startBtn');
-var onBtn = document.getElementById('onBtn');
-var offBtn = document.getElementById('offBtn');
-var circBtn = document.querySelectorAll('circ');
 
 /*----- event listeners -----*/
 
 startBtn.addEventListener('click', startGame);
-
 document.querySelector('section').addEventListener('click', handleCircClick);
+document.getElementById('c0').addEventListener('click', function() {
+    player.src = sounds[0];
+    player.play();
+});
+document.getElementById('c1').addEventListener('click', function() {
+    player.src = sounds[1];
+    player.play();
+});
+document.getElementById('c2').addEventListener('click', function() {
+    player.src = sounds[2];
+    player.play();
+});
+document.getElementById('c3').addEventListener('click', function() {
+    player.src = sounds[3];
+    player.play();
+});
+// function disableBtns(){
+//     document.getElementsByClassName('circ').disabled = true;
+// }
+// function enableBtns(){
+//     document.getElementsByClassName('circ').disabled = false;
+// }
 
 /*----- functions -----*/
 function initialize() {
@@ -26,7 +45,7 @@ function initialize() {
     render();
 }
 
-// responsible for transfering all state to the DOM
+// Responsible for transfering all state to the DOM
 function render() {
     startBtn.disabled = !gameOver;
     turnEl.textContent = gameOver ? 'Click start button.' : circleSequence.length;
@@ -48,7 +67,7 @@ function clearState() {
     }
 }
 
-// gameplay functionality
+// Gameplay functionality
 
 function handleCircClick(evt) {
     if (!evt.target.classList.contains('circ')) return;
@@ -77,11 +96,13 @@ function nextSequence() {
 
 function gameSequence() {
     ignoreClicks = true;
+    freezeBtns();
     circleSequence.forEach(function(seqIdx, idx) {
         setTimeout(function() {
             var elem = document.getElementById('c' + seqIdx);
             elem.className += ' activated';
-            // player.src = sounds[seqIdx];
+            player.src = sounds[seqIdx];
+            player.play(); 
             setTimeout(function() {
                 elem.classList.remove('activated');
                 player.pause();
@@ -90,6 +111,29 @@ function gameSequence() {
         }, 500 + (idx * 1000));
     });
     playerClicks = [];
+    unFreezeBtns();
+}
+
+function freezeBtns() {
+    var elems = document.getElementsByClassName('circ');
+    for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i];
+        elem.addEventListener('click', freezeClicks);
+    }
+}
+
+function unFreezeBtns() {
+    var elems = document.getElementsByClassName('circ');
+    for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i];
+        elem.removeEventListener('click', freezeClicks);
+    }
+}
+
+function freezeClicks(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
 }
 
 initialize();
