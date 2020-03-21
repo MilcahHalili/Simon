670845804ef91f6f0dbe75739a9b6a44cf4067e0
MilcahHalili <milcah.centeno@generalassemb.ly>
2020-03-20 19:01:05 -0700

    complete obj refactor

--- Simon/js/main.js
+++ Simon/js/main.js
@@ -1,33 +1,19 @@
 /*----- constants -----*/
-const sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
 const player = new Audio();
 
 /*----- app's state (variables) -----*/
-let circleSequence = [];
-let playerClicks = [];
-let gameOver, ignoreClicks;
-// refactor state and cached elem refs as properties in simon obj
 let simon = {
-	gameOver,
-	ignoreClicks,
+	gameOver: true,
+	ignoreClicks: true,
 	circleSequence: [],
 	playerClicks: [],
-	circles: {
-		c0: [document.getElementById('c0')],
-		c1: [document.getElementById('c1')],
-		c2: [document.getElementById('c2')],
-		c3: [document.getElementById('c3')]
-	},
+	circle: [document.getElementById('c0'), document.getElementById('c1'), document.getElementById('c2'), document.getElementById('c3')],
 	sounds: ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3']
 }
 
 /*----- cached element references -----*/
 let startBtn = document.getElementById('start-btn');
 let replayBtn = document.getElementById('replay-btn');
-const c0 = document.getElementById('c0');
-const c1 = document.getElementById('c1');
-const c2 = document.getElementById('c2');
-const c3 = document.getElementById('c3');
 
 /*----- event listeners -----*/
 startBtn.addEventListener('click', startGame);
@@ -36,19 +22,21 @@ document.querySelector('section').addEventListener('click', handleCircClick);
 
 /*----- functions -----*/
 function initialize() {
-	gameOver = true;
-	ignoreClicks = true;
+	simon.gameOver;
+	simon.ignoreClicks;
 	render();
 }
 
 function render() {
-	startBtn.disabled = !gameOver;
-	replayBtn.disabled = gameOver;
-	document.querySelector('h2').textContent = gameOver ? 'CLICK START' : circleSequence.length;
+	startBtn.disabled = !simon.gameOver;
+	replayBtn.disabled = simon.gameOver;
+	document.querySelector('h2').textContent = simon.gameOver
+		? 'CLICK START' 
+		: circleSequence.length;
 }
 
 function startGame() {
-	gameOver = false;
+	simon.gameOver = false;
 	circleSequence = [];
 	getNextTurn();
 	animateSequence();
@@ -60,17 +48,17 @@ function getNextTurn() {
 }
 
 function animateSequence() {
-	ignoreClicks = true;
+	simon.ignoreClicks = true;
 	circleSequence.forEach((seqIdx, idx) => {
 		setTimeout(() => {
-			let elem = document.getElementById('c' + seqIdx);
+			let elem = simon.circle[seqIdx];
 			elem.className += ' activated';
-			player.src = simon.sounds[seqIdx]; // sounds[seqIdx];
+			player.src = simon.sounds[seqIdx];
 			player.play();
 			setTimeout(() => {
 				elem.classList.remove('activated');
 				player.pause();
-				if (idx === circleSequence.length - 1) ignoreClicks = false;
+				if (idx === circleSequence.length - 1) simon.ignoreClicks = false;
 			}, 800)
 		}, 500 + (idx * 1000));
 	});
@@ -78,11 +66,11 @@ function animateSequence() {
 }
 
 function handleCircClick(evt) {
-	if (ignoreClicks) return;
+	if (simon.ignoreClicks) return;
 	if (!evt.target.classList.contains('circ')) return;
 	evt.className += ' activated';
 	let idx = parseInt(evt.target.id.replace('c', ''));
-	player.src = sounds[idx];
+	player.src = simon.sounds[idx];
 	player.play();
 	playerClicks.push(idx);
 	if (idx === circleSequence[playerClicks.length - 1]) {
@@ -91,7 +79,7 @@ function handleCircClick(evt) {
 			animateSequence();
 		}
 	} else {
-		gameOver = true;
+		simon.gameOver = true;
 	}
 	render();
 }
